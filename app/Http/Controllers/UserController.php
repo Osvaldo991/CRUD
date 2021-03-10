da25702041591db99786e33ab3b57fd25bc5e1e9
Osvaldo Sanchez <osvaldozurdo7@gmail.com>
2021-03-09 22:40:56 -0600

    video 5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,10 +1,33 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\User;
 
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function index()
+    {
+        $users = User::latest()->get();
+        return view('users.index', [
+            'users' => $users
+        ]);
+    }
+    public function store(Resquest $request)
+    {
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => $request->password,
+        ]);
+
+        return back();
+    }
+    public function destroy(User $user)
+    {
+        $user->delete();
+
+        return back();
+    }
 }
